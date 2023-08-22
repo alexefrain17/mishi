dcbff087b16683022b97cbf1ac0d9fa8be094f6b
alexefrain17 <1432461@senati.pe>
2023-08-21 20:03:25 -0500

    agregue suma

--- tabla/src/main/java/com/mycompany/tabla/Tabla.java
+++ tabla/src/main/java/com/mycompany/tabla/Tabla.java
@@ -12,5 +12,16 @@ public class Tabla {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("La suma");
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        int numero1 = scanner.nextInt();
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero2 = scanner.nextInt();
+        
+        int suma = numero1 + numero2;
+        System.out.println("la suma de " + numero1 + " + " + numero2 + " es: " + suma);
+        System.out.println();
+        
     }
 }
