4074d7b68a162c24f3a931c35f74770a66c10dcf
alexefrain17 <1432461@senati.pe>
2023-08-21 19:43:38 -0500

    agregue escanner

---
+++ tabla/src/main/java/com/mycompany/tabla/Tabla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tabla;
+
+/**
+ *
+ * @author HP
+ */
+public class Tabla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
